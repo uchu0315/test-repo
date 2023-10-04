21351df66741bb131040c814529d203bab6a9eab
Wataru Majima <10099071@003.fujifilm.com>
2023-10-05 08:30:29 +0900

    add print_hello

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_hello():
+    print("hello")
 
 
 def print_hi(name):
